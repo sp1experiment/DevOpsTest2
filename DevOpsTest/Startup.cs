ee3e084992919f1fd51bdd13e9b3f938594d46e4
sp1experiment <sp1experiment@gmail.com>
2016-12-20 13:17:46 -0500

    added everything

---
+++ DevOpsTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevOpsTest.Startup))]
+namespace DevOpsTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
